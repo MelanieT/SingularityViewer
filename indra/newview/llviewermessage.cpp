e1c3695182726f22287033d5a41ba51ffa1eddba
Melanie <melanie@t-data.com>
2015-11-15 19:54:46 +0100

    Cache the valeu of the only one parameter used rather than caching a class reference. Avoid multiple calls to the accessor.

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -5503,7 +5503,7 @@ void process_health_message(LLMessageSystem *mesgsys, void **user_data)
 
 void process_sim_stats(LLMessageSystem *msg, void **user_data)
 {	
-	LFSimFeatureHandler& features = LFSimFeatureHandler::instance();
+	float statsFactor = LFSimFeatureHandler::instance().simulatorFPSFactor();
 	S32 count = msg->getNumberOfBlocks("Stat");
 	for (S32 i = 0; i < count; ++i)
 	{
@@ -5517,34 +5517,34 @@ void process_sim_stats(LLMessageSystem *msg, void **user_data)
 			LLViewerStats::getInstance()->mSimTimeDilation.addValue(stat_value);
 			break;
 		case LL_SIM_STAT_FPS:
-			LLViewerStats::getInstance()->mSimFPS.addValue(stat_value / features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimFPS.addValue(stat_value / statsFactor);
 			break;
 		case LL_SIM_STAT_PHYSFPS:
-			LLViewerStats::getInstance()->mSimPhysicsFPS.addValue(stat_value / features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimPhysicsFPS.addValue(stat_value / statsFactor);
 			break;
 		case LL_SIM_STAT_AGENTUPS:
 			LLViewerStats::getInstance()->mSimAgentUPS.addValue(stat_value);
 			break;
 		case LL_SIM_STAT_FRAMEMS:
-			LLViewerStats::getInstance()->mSimFrameMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimFrameMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_NETMS:
-			LLViewerStats::getInstance()->mSimNetMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimNetMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_SIMOTHERMS:
-			LLViewerStats::getInstance()->mSimSimOtherMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimSimOtherMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_SIMPHYSICSMS:
-			LLViewerStats::getInstance()->mSimSimPhysicsMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimSimPhysicsMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_AGENTMS:
-			LLViewerStats::getInstance()->mSimAgentMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimAgentMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_IMAGESMS:
-			LLViewerStats::getInstance()->mSimImagesMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimImagesMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_SCRIPTMS:
-			LLViewerStats::getInstance()->mSimScriptMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimScriptMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_NUMTASKS:
 			LLViewerStats::getInstance()->mSimObjects.addValue(stat_value);
@@ -5620,13 +5620,13 @@ void process_sim_stats(LLMessageSystem *msg, void **user_data)
 			LLViewerStats::getInstance()->mPhysicsMemoryAllocated.addValue(stat_value);
 			break;
 		case LL_SIM_STAT_SIMSPARETIME:
-			LLViewerStats::getInstance()->mSimSpareMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimSpareMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_SIMSLEEPTIME:
-			LLViewerStats::getInstance()->mSimSleepMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimSleepMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_IOPUMPTIME:
-			LLViewerStats::getInstance()->mSimPumpIOMsec.addValue(stat_value * features.simulatorFPSFactor());
+			LLViewerStats::getInstance()->mSimPumpIOMsec.addValue(stat_value * statsFactor);
 			break;
 		case LL_SIM_STAT_PCTSCRIPTSRUN:
 			LLViewerStats::getInstance()->mSimPctScriptsRun.addValue(stat_value);
